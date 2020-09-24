af38b7a2ef59778f0d3110d81751c9c44f348656
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-09-24 09:54:03 +0300

    formated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,11 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                    @if(session('status'))
+                        <div
+                            class="alert alert-success"
+                            role="alert"
+                        >
                             {{ session('status') }}
                         </div>
                     @endif
